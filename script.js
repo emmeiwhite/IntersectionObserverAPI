729aa770498c8295118766b1090e70363811f3dc
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-10-05 08:51:33 +0530

    UI animation on scroll - triggered animations

--- script.js
+++ script.js
@@ -14,4 +14,44 @@ const obsOptions = {
 };
 
 const observer = new IntersectionObserver(callback, obsOptions);
-observer.observe(header);
+// observer.observe(header);
+
+// Observing the red div to use our animation on
+
+const box = document.querySelector("div");
+const para = document.querySelector(".para");
+
+const callbackFunction = function (entries) {
+  const [entry] = entries; // first element in the entries array
+  console.log(entry);
+
+  // Total 4 intersection cases:
+
+  // While Scrolling down 2 intersection cases
+
+  if (entry.isIntersecting === true && entry.intersectionRatio > 0.5) {
+    window.addEventListener("scroll", function () {
+      console.log("Entered the view port the first time");
+
+      let boxTop = box.getBoundingClientRect().top;
+      const boxHeight = box.getBoundingClientRect().height;
+      //   console.log(boxTop);
+      let boxInViewHeight = window.innerHeight - boxTop;
+
+      let scrollPercentage = parseInt(
+        (boxInViewHeight / boxHeight) * 100 - 100
+      );
+      console.log(`translateX(${scrollPercentage}%)`);
+
+      para.style.transform = `translate(${scrollPercentage}%)`;
+    });
+  }
+};
+
+const boxObserver = new IntersectionObserver(callbackFunction, {
+  threshold: 0.5,
+  root: null,
+  //   rootMargin: "-100px",
+});
+
+boxObserver.observe(box);
